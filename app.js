73c914e8cc1e4e80a15c069d4d39c986ab9f7da6
projitsarkar595-ai <projitsarkar595@gmail.com>
2026-06-21 00:19:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
 // Home page buttons
 
-document.getElementById("app").innerHTML = `
-
-<button onclick="location.href='players.html'">
-Player Registration
-</button>
-
-<button onclick="location.href='draw.html'">
-Random Draw
-</button>
-
-<button onclick="location.href='nextRound.html'">
-Next Round
-</button>
-
-<button onclick="location.href='champion.html'">
-Champion Screen
-</button>
-
-`;
+document.getElementById("app").innerHTML = "<h2>Hello FIFA</h2>";
